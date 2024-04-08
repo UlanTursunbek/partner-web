73cbfbb12eb233aef0a67b149c3bbc3c39c8b3ba
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-04-08 17:00:29 +0600

    Create quick acces menu

---
+++ src/shared/Icons/IconChevronDown.tsx
@@ -0,0 +1,16 @@
+import { createIcon } from '@chakra-ui/react'
+
+export const IconChevronDown = createIcon({
+  displayName: 'IconChevronDown',
+  viewBox: '0 0 24 24',
+  path: (
+    <path
+      d="M6 9L12 15L18 9"
+      stroke="currentColor"
+      fill="none"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  ),
+})
