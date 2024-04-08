7769179ff0727b003dee5964afd23d5e73469741
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-04-08 17:00:12 +0600

    Update styles in theme

--- src/app/theme/components/Button.tsx
+++ src/app/theme/components/Button.tsx
@@ -13,6 +13,28 @@ const primary = defineStyle({
   justifyContent: 'center',
 })
 
+const outline = defineStyle({
+  backgroundColor: 'transparent',
+  color: 'black',
+  borderRadius: '6px',
+  border: '1px solid #CBD5E1',
+  padding: '8px 16px',
+  margin: '0',
+  fontWeight: '700',
+  fontSize: '14px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+const md = defineStyle({
+  fontSize: '14px',
+  fontWeight: '700',
+  borderRadius: '6px',
+  padding: '8px 16px',
+})
+
 export const buttonStyle = defineStyleConfig({
-  variants: { primary },
+  variants: { primary, outline },
+  sizes: { md },
 })
--- src/app/theme/index.ts
+++ src/app/theme/index.ts
@@ -1,11 +1,17 @@
 import { extendBaseTheme } from '@chakra-ui/react'
 
 import { buttonStyle } from './components/Button'
+import { drawerStyle } from './components/Drawer'
+import { inputStyle } from './components/Input'
+import { menuStyle } from './components/Menu'
 import { colors } from './colors'
 
 export const customTheme = extendBaseTheme({
   components: {
     Button: buttonStyle,
+    Drawer: drawerStyle,
+    Input: inputStyle,
+    Menu: menuStyle,
   },
   colors,
   styles: {
