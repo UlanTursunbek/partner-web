a0f831e386bc5cd9d5eb0d0ddc6d7e02fe25ea1a
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-04-08 12:08:35 +0600

    Set absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/partner-web/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
