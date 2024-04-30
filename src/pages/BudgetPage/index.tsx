1e730ebe36db765749a716fa6ab7d1ff175d093e
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-04-30 20:04:47 +0600

    Render Doughnut chart for expenses

--- src/pages/BudgetPage/index.tsx
+++ src/pages/BudgetPage/index.tsx
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import { BudgetChart } from '@/widgets'
 import {
   Tab,
@@ -8,6 +10,31 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react'
+
+const MOCK_EXPENSES = [
+  { category: 'Food', value: 1000, color: 'red' },
+  { category: 'Bar', value: 140, color: 'green' },
+  { category: 'Car', value: 1000, color: 'black' },
+  { category: 'Baby', value: 500, color: 'purple' },
+  { category: 'Cafe', value: 100, color: 'blue' },
+  { category: 'Food', value: 200, color: 'violet' },
+]
+
+const chunks = _.chunk(MOCK_EXPENSES, 2)
+
+const normalizedData = {
+  datasets: [
+    {
+      data: chunks[0].map((it) => it.value),
+      backgroundColor: chunks[0].map((it) => it.color),
+    },
+    {
+      data: chunks[1].map((it) => it.value),
+      backgroundColor: chunks[1].map((it) => it.color),
+    },
+  ],
+  labels: MOCK_EXPENSES.map((it) => it.category),
+}
 
 export const BudgetPage = () => {
   return (
@@ -24,7 +51,10 @@ export const BudgetPage = () => {
 
         <TabPanels>
           <TabPanel>
-            <BudgetChart />
+            <BudgetChart
+              data={normalizedData}
+              totalValue={_.sumBy(MOCK_EXPENSES, 'value')}
+            />
           </TabPanel>
           <TabPanel>
             <p>two!</p>
